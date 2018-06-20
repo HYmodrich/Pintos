d2c97a0df253bcdbf7b968e3d1c1728eed78527c
HYmodrich <lwj0337@naver.com>
2018-06-20 13:24:15 +0900

    vm

---
+++ vm/src/vm/build/userprog/syscall.d
@@ -0,0 +1,10 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+ ../../threads/synch.h ../../lib/kernel/list.h ../../lib/stdbool.h \
+ ../../lib/stddef.h ../../lib/stdint.h ../../userprog/process.h \
+ ../../threads/thread.h ../../lib/debug.h ../../lib/kernel/hash.h \
+ ../../lib/kernel/list.h ../../lib/stdio.h ../../lib/stdarg.h \
+ ../../lib/kernel/stdio.h ../../lib/syscall-nr.h ../../lib/string.h \
+ ../../threads/interrupt.h ../../devices/shutdown.h ../../devices/input.h \
+ ../../filesys/filesys.h ../../filesys/off_t.h ../../filesys/file.h \
+ ../../userprog/pagedir.h ../../threads/vaddr.h ../../threads/loader.h \
+ ../../vm/page.h
